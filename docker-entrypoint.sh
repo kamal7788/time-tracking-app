4fcb09ca117d913277fcd18167666de5bffe83b0
kamalparajuli <kamal@kamalparajuli.com>
2026-06-17 15:42:26 +0800

    Skip prisma generate in db push to avoid permission errors

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Run database migrations and seed
-./node_modules/.bin/prisma db push --accept-data-loss
+./node_modules/.bin/prisma db push --accept-data-loss --skip-generate
 ./node_modules/.bin/tsx prisma/seed.ts
 
 # Start the application
